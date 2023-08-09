d16b2db591d559e3a65191c4f4d5e5d7bc8a1592
Dani-dornas <lauby.dd@gmail.com>
2023-08-08 21:24:03 -0300

    primeira aula

--- src/exercicios/exer10.ts
+++ src/exercicios/exer10.ts
@@ -1,5 +1,5 @@
-const sum:Function = (a:number,b:number):number => a + b;
-const dif:Function = (a:number,b:number):number => a - b;
+const sum = (a:number,b:number):number => a + b;
+const dif = (a:number,b:number):number => a - b;
 const operacao = (f:Function,a:number,b:number) => f(a,b);
 
 console.log("5 + 3:", operacao(sum,5,3));
